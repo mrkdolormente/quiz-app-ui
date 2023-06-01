e452c5eaf085985c0d21696e37363e189ff4378e
Mark Dolormente <markjosephandinodolormente@gmail.com>
2023-06-01 21:31:32 +0800

    [refactor/components]: allow to disable animation and customize icon

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -12,8 +12,10 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonComponent {
+  @Input() icon: string = 'arrow_forward';
   @Input() isDisabled: boolean = false;
   @Input() text: string = '';
+  @Input() withAnimation: boolean = true;
 
   @Output() clickEvent = new EventEmitter();
   constructor() {}
