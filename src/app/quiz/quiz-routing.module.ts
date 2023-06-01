99d4d678d778710aa594884796b86cfe86b2efc2
Mark Dolormente <markjosephandinodolormente@gmail.com>
2023-06-01 13:02:56 +0800

    [feature/result]: include quiz result in the routing

--- src/app/quiz/quiz-routing.module.ts
+++ src/app/quiz/quiz-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { QuizComponent } from './quiz.component';
+import { QuizResultComponent } from './quiz-result/quiz-result.component';
 
 const routes: Routes = [
   {
     path: '',
     component: QuizComponent,
+  },
+  {
+    path: 'result',
+    component: QuizResultComponent,
   },
 ];
 
--- src/app/quiz/quiz.module.ts
+++ src/app/quiz/quiz.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
 import { QuizComponent } from './quiz.component';
 import { QuizRoutingModule } from './quiz-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { QuestionCardComponent } from './components/question-card/question-card.component';
+import { QuizResultComponent } from './quiz-result/quiz-result.component';
 
 @NgModule({
-  declarations: [QuizComponent, QuestionCardComponent],
-  imports: [CommonModule, QuizRoutingModule, SharedModule],
+  declarations: [QuizComponent, QuestionCardComponent, QuizResultComponent],
+  imports: [CommonModule, QuizRoutingModule, SharedModule, MatIconModule, MatProgressSpinnerModule],
 })
 export class QuizModule {}
