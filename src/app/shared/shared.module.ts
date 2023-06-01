5a1fdd8d7b26a2c1136b18fe05b229d68c44acb2
Mark Dolormente <markjosephandinodolormente@gmail.com>
2023-06-01 19:58:34 +0800

    [fix/functional]: fix logo redirect

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { WrapperComponent } from './wrapper/wrapper.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [TopBarComponent, WrapperComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [TopBarComponent, WrapperComponent],
 })
 export class SharedModule {}
