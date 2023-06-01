3f7c3b632ee1e71b231141ecdc1eff0690396c47
Mark Dolormente <markjosephandinodolormente@gmail.com>
2023-06-01 18:30:45 +0800

    [feature/state-manage]: adjust spinner value based on average

--- src/app/quiz/quiz-result/quiz-result.component.ts
+++ src/app/quiz/quiz-result/quiz-result.component.ts
@@ -4,7 +4,7 @@ import { ThemePalette } from '@angular/material/core';
 import { QuizFacade } from 'src/app/state/quiz/quiz.facade';
 import { QuizResult } from 'src/app/models/quiz.model';
 
-import { Observable } from 'rxjs';
+import { Observable, concatMap, interval, map, take, tap } from 'rxjs';
 import { RxEffects } from '@rx-angular/state/effects';
 
 @Component({
@@ -15,12 +15,28 @@ import { RxEffects } from '@rx-angular/state/effects';
 export class QuizResultComponent implements OnInit {
   spinnerColor: ThemePalette = 'primary';
   spinnerMode: ProgressSpinnerMode = 'determinate';
-  spinnerValue: number = 50;
+  spinnerValue: number = 0;
 
   quizResult$: Observable<QuizResult | null>;
 
-  constructor(private readonly _quizFacade: QuizFacade) {
+  constructor(
+    private readonly _quizFacade: QuizFacade,
+    private readonly _effects: RxEffects
+  ) {
     this.quizResult$ = this._quizFacade.quizResult$;
+
+    this._effects.register(
+      this.quizResult$.pipe(
+        map((result) => Math.round(result?.average || 0)),
+        concatMap((average) =>
+          interval(10).pipe(
+            take(average),
+            tap(() => this.spinnerValue++)
+          )
+        )
+      )
+    );
+    interval();
   }
 
   ngOnInit(): void {}
