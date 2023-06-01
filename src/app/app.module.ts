3d65cbd36713f0f9b9a90b2e76d758b7ce7db251
Mark Dolormente <markjosephandinodolormente@gmail.com>
2023-06-01 18:11:35 +0800

    [feature/state-management]: initialize state management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,42 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import {
+  DEFAULT_ROUTER_FEATURENAME,
+  RouterState,
+  StoreRouterConnectingModule,
+  routerReducer,
+} from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot(
+      {
+        [DEFAULT_ROUTER_FEATURENAME]: routerReducer,
+      },
+      {
+        runtimeChecks: {
+          strictStateImmutability: true,
+          strictActionImmutability: true,
+          strictStateSerializability: true,
+          strictActionSerializability: true,
+          strictActionWithinNgZone: true,
+          strictActionTypeUniqueness: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({
+      routerState: RouterState.Minimal,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
