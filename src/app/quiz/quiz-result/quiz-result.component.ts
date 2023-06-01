fdb986880841290305ba1c909e4934b9a1f58009
Mark Dolormente <markjosephandinodolormente@gmail.com>
2023-06-01 18:15:22 +0800

    [feature/state-management]: apply state management to quiz pages

--- src/app/quiz/quiz-result/quiz-result.component.ts
+++ src/app/quiz/quiz-result/quiz-result.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
 import { ThemePalette } from '@angular/material/core';
+import { QuizFacade } from 'src/app/state/quiz/quiz.facade';
+import { QuizResult } from 'src/app/models/quiz.model';
+
+import { Observable } from 'rxjs';
+import { RxEffects } from '@rx-angular/state/effects';
 
 @Component({
   selector: 'app-quiz-result',
   templateUrl: './quiz-result.component.html',
+  providers: [RxEffects],
 })
 export class QuizResultComponent implements OnInit {
   spinnerColor: ThemePalette = 'primary';
   spinnerMode: ProgressSpinnerMode = 'determinate';
   spinnerValue: number = 50;
-  constructor() {}
+
+  quizResult$: Observable<QuizResult | null>;
+
+  constructor(private readonly _quizFacade: QuizFacade) {
+    this.quizResult$ = this._quizFacade.quizResult$;
+  }
 
   ngOnInit(): void {}
 }
