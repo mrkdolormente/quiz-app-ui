f8521d5a3a28812512c28b5850d9ea8afaaf95bf
Mark Dolormente <markjosephandinodolormente@gmail.com>
2023-06-02 17:59:45 +0800

    [test/unit-test]: add NumberToAlphabetPipe unit test

--- src/app/pipes/number-to-alphabet.pipe.spec.ts
+++ src/app/pipes/number-to-alphabet.pipe.spec.ts
@@ -1,8 +1,37 @@
 import { NumberToAlphabetPipe } from './number-to-alphabet.pipe';
 
 describe('NumberToAlphabetPipe', () => {
+  let pipe: NumberToAlphabetPipe;
+
+  beforeEach(() => {
+    pipe = new NumberToAlphabetPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new NumberToAlphabetPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return A when value is 0', () => {
+    const character = pipe.transform(0);
+
+    expect(character).toEqual('A');
+  });
+
+  it('should return B when value is 1', () => {
+    const character = pipe.transform(0);
+
+    expect(character).toEqual('B');
+  });
+
+  it('should return C when value is 2', () => {
+    const character = pipe.transform(2);
+
+    expect(character).toEqual('C');
+  });
+
+  it('should return D when value is 3', () => {
+    const character = pipe.transform(3);
+
+    expect(character).toEqual('D');
   });
 });
