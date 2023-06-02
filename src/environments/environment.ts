7cfbf4d841a214824dab3137dbedc6ed6fabae1b
Mark Dolormente <markjosephandinodolormente@gmail.com>
2023-06-02 15:03:26 +0800

    [feature/recommendation]: add test and train url in env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  testAndTrainUrl: 'https://www.testandtrain.com/',
 };
 
 /*
