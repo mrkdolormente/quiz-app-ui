9c338b81045ccfae59c09d5ff701c5cd8fb771d0
Mark Dolormente <markjosephandinodolormente@gmail.com>
2023-06-02 18:02:22 +0800

    [refactor/quiz]: rename quiz result guard class

--- src/app/layouts/quiz/quiz-routing.module.ts
+++ src/app/layouts/quiz/quiz-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { QuizComponent } from './quiz.component';
 import { QuizResultComponent } from './quiz-result/quiz-result.component';
-import { QuizResultGuardGuard } from '../../guards/quiz-result-guard.guard';
+import { QuizResultGuard } from '../../guards/quiz-result-guard.guard';
 
 const routes: Routes = [
   {
@@ -12,7 +12,7 @@ const routes: Routes = [
   {
     path: 'result',
     component: QuizResultComponent,
-    canActivate: [QuizResultGuardGuard],
+    canActivate: [QuizResultGuard],
   },
 ];
 
