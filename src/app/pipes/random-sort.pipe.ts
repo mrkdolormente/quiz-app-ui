156b9420dee720faac9961e29cb5fbfc1640fbae
Mark Dolormente <markjosephandinodolormente@gmail.com>
2023-06-03 20:12:30 +0800

    [chore/comments]: add comment to number to random sort pipe

--- src/app/pipes/random-sort.pipe.ts
+++ src/app/pipes/random-sort.pipe.ts
@@ -4,6 +4,11 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'randomSort',
 })
 export class RandomSortPipe implements PipeTransform {
+  /**
+   * @param value list to randomize
+   * @param args other options
+   * @returns randomize list
+   */
   transform(value: Array<unknown>, ...args: unknown[]): Array<any> {
     const list = [...value];
 
